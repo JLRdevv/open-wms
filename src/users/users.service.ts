878b1ae6e43f72d1fb9a6c008ffa5de25b87a84d
unknown <joaolucas3406@gmail.com>
2026-01-25 20:27:36 -0300

    refactor(auth): separated users service and repository responsabilities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import {
   BadRequestException,
   Injectable,
+  InternalServerErrorException,
+  Logger,
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
@@ -12,11 +14,15 @@ import { type UserSession } from '@thallesp/nestjs-better-auth';
 import { RotatePasswordDto } from './dtos/rotate-password';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { canManageRole, RoleLevel } from 'src/lib/auth/role.util';
-import e from 'express';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  private readonly logger = new Logger(UsersService.name);
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly usersRepository: UsersRepository,
+  ) {}
 
   async createEmployee(newEmployee: CreateEmployeeDto, currentUser: User) {
     if (!canManageRole(currentUser.role, newEmployee.role)) {
@@ -25,7 +31,15 @@ export class UsersService {
       );
     }
 
-    // TO DO: warehouse assignment for operators and managers
+    let registerToWarehouse = false;
+    if (RoleLevel[newEmployee.role] <= RoleLevel[Role.MANAGER]) {
+      if (!newEmployee.warehouseId) {
+        throw new BadRequestException(
+          `The role ${newEmployee.role} requires the employee to be assigned to at least one warehouse, provide a warehouseId.`,
+        );
+      }
+      registerToWarehouse = true;
+    }
 
     UserUtils.passwordValidation(newEmployee.password, newEmployee.role);
 
@@ -39,22 +53,31 @@ export class UsersService {
     const email = newEmployee.email || `${newEmployee.username}@local.com`;
 
     try {
-      await auth.api.signUpEmail({
-        body: {
-          email,
-          password: newEmployee.password,
-          name: newEmployee.name,
-          role: newEmployee.role,
-          username: newEmployee.username,
-          createdBy: currentUser.id,
-        },
-        headers: new Headers({
-          [process.env.INTERNAL_HEADER_NAME!]: process.env.INTERNAL_SECRET!,
-        }),
+      await this.prisma.$transaction(async () => {
+        const signUpResponse = await auth.api.signUpEmail({
+          body: {
+            email,
+            password: newEmployee.password,
+            name: newEmployee.name,
+            role: newEmployee.role,
+            username: newEmployee.username,
+            createdBy: currentUser.id,
+          },
+          headers: new Headers({
+            [process.env.INTERNAL_HEADER_NAME!]: process.env.INTERNAL_SECRET!,
+          }),
+        });
+        if (registerToWarehouse) {
+          await this.usersRepository.connectWarehouse(
+            signUpResponse.user.id,
+            newEmployee.warehouseId!,
+          );
+        }
       });
-      return { created: true };
+      return { success: true, message: 'Employee created successfully.' };
     } catch (error) {
-      throw error;
+      this.logger.error('Error creating employee', error);
+      throw new InternalServerErrorException();
     }
   }
 
@@ -74,25 +97,24 @@ export class UsersService {
     UserUtils.passwordValidation(body.newPassword, user.role);
 
     try {
-      await auth.api.changePassword({
-        body: {
-          currentPassword: body.currentPassword,
-          newPassword: body.newPassword,
-        },
-        headers,
-      });
-
-      await this.prisma.user.update({
-        where: { id: user.id },
-        data: { shouldRotatePassword: false },
-      });
+      await Promise.all([
+        auth.api.changePassword({
+          body: {
+            currentPassword: body.currentPassword,
+            newPassword: body.newPassword,
+          },
+          headers,
+        }),
+        this.usersRepository.enableRotatePassword(user.id),
+      ]);
 
       return {
         success: true,
         message: 'Password changed successfully.',
       };
     } catch (error) {
-      throw error;
+      this.logger.error('Error rotating password', error);
+      throw new InternalServerErrorException();
     }
   }
 
@@ -107,9 +129,8 @@ export class UsersService {
       throw new BadRequestException('You cannot change your own role.');
     }
 
-    const employee = await this.prisma.user.findUnique({
-      where: { id: employeeId },
-      include: { warehouses: true },
+    const employee = await this.usersRepository.findById(employeeId, {
+      warehouses: true,
     });
 
     if (!employee) {
@@ -123,7 +144,7 @@ export class UsersService {
     }
 
     //users bellow or equal to manager must be assigned to at least one warehouse
-    let registerWarehouse = false;
+    let registerToWarehouse = false;
     if (RoleLevel[newRole] <= RoleLevel[Role.MANAGER]) {
       if (employee.warehouses.length === 0 && !warehouseId) {
         throw new BadRequestException(
@@ -136,32 +157,22 @@ export class UsersService {
 
       //validate warehouse existence
       if (!warehouse) throw new BadRequestException('Warehouse not found!');
-      registerWarehouse = true;
+      registerToWarehouse = true;
     }
 
     try {
-      await this.prisma.$transaction(async (prisma) => {
-        await prisma.user.update({
-          where: { id: employeeId },
-          data: { role: newRole },
-        });
-        if (registerWarehouse) {
-          await this.prisma.user.update({
-            where: { id: employeeId },
-            data: {
-              warehouses: {
-                connect: { id: warehouseId },
-              },
-            },
-          });
-        }
-      });
+      await this.usersRepository.updateRole(
+        employeeId,
+        newRole,
+        registerToWarehouse ? warehouseId : undefined,
+      );
       return {
         message: 'Role updated successfully.',
         newRole,
       };
     } catch (error) {
-      throw error;
+      this.logger.error('Error updating role', error);
+      throw new InternalServerErrorException();
     }
   }
 }
